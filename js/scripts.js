b2f4930c66dc029c01170bd4611edc9eb50e7ffc
Maja Jelenik <maja.jelenik@vp.pl>
2018-05-28 14:49:34 +0200

    Change js files

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,8 @@ var list = document.getElementById("list");
 var add = document.getElementById("addElem");
 var item = 0;
 
-addElem.addEventListener(addElem, function(e) {});
-
 add.addEventListener("click", function() {
 	var element = document.createElement("li");
-	element.innerHTML = "item";
 	var itemsByTagName = document.getElementsByTagName("list");
 
 	console.log(list.length);
